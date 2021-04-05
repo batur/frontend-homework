30d01b6571dff28bcd0fac5ada0b6df8aa206424
cosakin <halidbaturakkurt@gmail.com>
2021-04-05 17:07:52 +0300

    Bootstrap CDN and node-sass added

---
+++ src/pages/home/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <h1>Invoicing App</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
